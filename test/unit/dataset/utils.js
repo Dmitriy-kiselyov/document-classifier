cd5db89433e31057e7efc70127825b13dbeb9fe9
Dmitriy-Kiselyov <dmitriy.penetrator@gmail.com>
2018-03-29 09:22:57 +0300

    Refactor dataset utils

--- test/unit/dataset/utils.js
+++ test/unit/dataset/utils.js
@@ -1,11 +1,27 @@
 'use strict';
 
+const fs = require('fs-extra');
+const path = require('path');
+
 const utils = require('lib/dataset/utils');
 
 describe('dataset/utils', () => {
     const sandbox = sinon.createSandbox();
 
     afterEach(() => sandbox.restore());
+
+    it('get files', async () => {
+        sandbox.stub(fs, 'readdir').withArgs('folder').resolves(['file1', 'subfolder', 'file2']);
+        sandbox.stub(path, 'resolve').callsFake((folder, file) => `${folder}/${file}`);
+        sandbox.stub(fs, 'stat')
+            .withArgs('folder/file1').resolves({isFile: () => true})
+            .withArgs('folder/file2').resolves({isFile: () => true})
+            .withArgs('folder/subfolder').resolves({isFile: () => false});
+
+        const files = await utils.getFiles('folder');
+
+        assert.deepEqual(files, ['folder/file1', 'folder/file2']);
+    });
 
     it('split files', () => {
         sandbox.stub(Math, 'random')
