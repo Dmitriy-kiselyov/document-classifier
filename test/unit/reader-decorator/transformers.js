422ed6599cafa8db102af04fe70244d4513714a4
Dmitriy-Kiselyov <dmitriy.penetrator@gmail.com>
2018-04-12 11:07:16 +0300

    Новое разбиение слов

--- test/unit/reader-decorator/transformers.js
+++ test/unit/reader-decorator/transformers.js
@@ -1,11 +1,19 @@
 'use strict';
 
-const {TO_LOWER_CASE} = require('lib/reader-decorator/transformers');
+const {TO_LOWER_CASE, TRIM} = require('lib/reader-decorator/transformers');
 
 describe('reader-decorator/transformers', () => {
     describe('TO_LOWER_CASE', () => {
         it('should transform all letters to lower case', () => {
             assert.equal(TO_LOWER_CASE('toLower123Case'), 'tolower123case');
+        });
+    });
+
+    describe('TRIM', () => {
+        it('should trim symbols than should appear only on the end', () => {
+            const delim = '_.:-?!`"\'~#;*';
+
+            assert.equal(TRIM(`${delim}A${delim}B${delim}`), `A${delim}B`);
         });
     });
 });
