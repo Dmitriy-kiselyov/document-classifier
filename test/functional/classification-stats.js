fed0ec0d8f229d1b947e31e4b23307820202bbdc
Dmitriy-Kiselyov <dmitriy.penetrator@gmail.com>
2018-04-13 00:00:02 +0300

    Add percent statistics

--- test/functional/classification-stats.js
+++ test/functional/classification-stats.js
@@ -1,11 +1,37 @@
 'use strict';
 
 const Promise = require('bluebird');
+const path = require('path');
+const _ = require('lodash');
 
 const Classifier = require('../../lib/classifier');
 const utils = require('./utils');
 
 const root = 'C:\\Users\\dmitr\\Desktop\\20_newsgroup';
+
+const printStats = (stats) => {
+    const formatPercent = (yes, total) => {
+        return _.floor(yes / total, 2);
+    };
+
+    stats.forEach((test) => test.folder = path.basename(test.folder));
+    stats.sort((test1, test2) => test1.folder.localeCompare(test2.folder));
+
+    let yesSum = 0;
+    let totalSum = 0;
+    stats.forEach(({folder, stat}) => {
+        const total = stat.yes + stat.no;
+        const percent = formatPercent(stat.yes, total);
+
+        yesSum += stat.yes;
+        totalSum += total;
+
+        console.log(folder, stat, `${percent}%`);
+    });
+
+    const percent = formatPercent(yesSum, totalSum);
+    console.log(`Classification rate: ${percent}%`);
+};
 
 const main = async (rate) => {
     console.time('classification');
@@ -16,11 +42,15 @@ const main = async (rate) => {
     await classifier.prepare(folders, rate);
     console.log('Prepare complete');
 
+    const stats = [];
     await Promise.map(folders, async (folder) => {
-        const ans = await classifier.test(folder);
-        console.log(folder, ans);
+        const stat = await classifier.test(folder);
+        console.log(folder, stat);
+        stats.push({folder, stat});
     });
 
+    console.log('------------------------------');
+    printStats(stats);
     console.timeEnd('classification');
 };
 
